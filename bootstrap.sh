b38103953241ca2b68888cb31ed92b3a34558411
tsk takezawa <tsk.take815@gmail.com>
2015-04-21 01:18:49 +0900

    Update prompt theme

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,5 @@
-cd ~/
+#!/bin/bash
+set -e
 
 # init dir
 mkdir ~/.config
@@ -6,3 +7,9 @@ mkdir -p ~/.vim/bundle
 
 # neobundle install
 git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+
+# oh-my-zsh-theme install
+if [[ ! -f ~/.oh-my-zsh/themes/t4.zsh-theme ]]; then
+    info "Install oh-my-zsh-theme"
+    curl -fsSL https://raw.github.com/to4iki/oh-my-zsh-t4-theme/master/t4.zsh-theme >> ~/.oh-my-zsh/themes/t4.zsh-theme
+fi
