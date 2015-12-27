94a9d10d099a8b2805d86e8b752704a45055a583
tsk takezawa <tsk.take815@gmail.com>
2015-12-27 17:59:10 +0900

    Change zsh-theme

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,9 +7,3 @@ mkdir -p ~/.vim/bundle
 
 # neobundle install
 git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
-
-# oh-my-zsh-theme install
-if [[ ! -f ~/.oh-my-zsh/themes/t4.zsh-theme ]]; then
-    info "Install oh-my-zsh-theme"
-    curl -fsSL https://raw.github.com/to4iki/oh-my-zsh-t4-theme/master/t4.zsh-theme >> ~/.oh-my-zsh/themes/t4.zsh-theme
-fi
