81379032a04d7a7a36b3e2c0cb95dbe18d2416d7
Toshiki Takezawa <tsk.take815@gmail.com>
2019-04-10 18:13:23 +0900

    [fish] Replace `-gx` -> `-x`

--- fish/config.fish
+++ fish/config.fish
@@ -1,19 +1,19 @@
 ## PATH
 # git
-set -gx PATH /usr/local/share/git-core/contrib/diff-highlight $PATH
+set -x PATH /usr/local/share/git-core/contrib/diff-highlight $PATH
 # Go
-set -gx GOPATH $HOME
+set -x GOPATH $HOME
 # rbenv
-set -gx RBENV_ROOT $HOME/.rbenv/
+set -x RBENV_ROOT $HOME/.rbenv/
 # z
-set -gx Z_SCRIPT_PATH (brew --prefix)/etc/profile.d/z.sh
+set -x Z_SCRIPT_PATH (brew --prefix)/etc/profile.d/z.sh
 # NeoVim
-set -gx XDG_CONFIG_HOME $HOME/.config
+set -x XDG_CONFIG_HOME $HOME/.config
 # Local scripts
-set -gx PATH $HOME/src/github.com/to4iki/dotfiles/bin $PATH
+set -x PATH $HOME/src/github.com/to4iki/dotfiles/bin $PATH
 
 ## EDITOR
-set -U EDITOR vim
+set -x EDITOR vim
 
 ## LANGUAGE
 set -x LANG ja_JP.UTF-8
