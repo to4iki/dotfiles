340211b2ccbbf2d4177f78647a3e78b9f91d58bb
Toshiki Takezawa <tsk.take815@gmail.com>
2026-01-25 17:29:34 +0900

    rafactor: cleanup

--- dot_config/zsh/functions.zsh
+++ dot_config/zsh/functions.zsh
@@ -19,33 +19,6 @@ fzf_branch() {
   zle reset-prompt
 }
 
-# fzf git diff between two commits
-fzf_difit() {
-  local from_commit to_commit from_hash to_hash
-
-  from_commit=$(git log --oneline --decorate -100 --color=always | \
-    fzf \
-      --ansi \
-      --header "> difit \$TO \$FROM~1" \
-      --prompt "Select \$FROM>" \
-      --preview 'git log --oneline --decorate --color=always -1 {1}' \
-      --preview-window=top:3:wrap
-  ) || return
-  from_hash="${from_commit%% *}"
-
-  to_commit=$(git log --oneline --decorate -100 --color=always $from_hash~1.. | \
-    fzf \
-      --ansi \
-      --header "> difit \$TO $from_hash~1" \
-      --prompt "Select \$TO>" \
-      --preview 'git log --oneline --decorate --color=always -1 {1}' \
-      --preview-window=top:3:wrap
-  ) || return
-  to_hash="${to_commit%% *}"
-
-  difit "$to_hash" "$from_hash~1"
-}
-
 # git-wt with fzf integration
 wt() {
   git wt $(git wt | tail -n +2 | fzf | awk '{print $(NF-1)}')
