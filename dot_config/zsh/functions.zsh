855e43d8642d4d7b2b6bc746edfe42d11f07eb53
Toshiki Takezawa <tsk.take815@gmail.com>
2026-01-30 01:41:43 +0900

    refactor: remove unnecessary code

--- dot_config/zsh/functions.zsh
+++ dot_config/zsh/functions.zsh
@@ -8,17 +8,6 @@ fzf_ghq() {
   zle reset-prompt
 }
 
-# fzf git branch switcher
-fzf_branch() {
-  local branches=$(git branch --all | grep -v HEAD | sed 's/^[* ] //' | sed 's#^remotes/origin/##')
-  local selected=$(echo "$branches" | sort -u | fzf --query "$LBUFFER")
-  if [ -n "$selected" ]; then
-    BUFFER="git switch $selected"
-    zle accept-line
-  fi
-  zle reset-prompt
-}
-
 # git-wt with fzf integration
 wt() {
   git wt $(git wt | tail -n +2 | fzf | awk '{print $(NF-1)}')
