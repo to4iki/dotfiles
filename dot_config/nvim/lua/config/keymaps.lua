fba99bf9f9ef9879961f63a83626885302c6093a
Toshiki Takezawa <tsk.take815@gmail.com>
2026-03-06 18:28:58 +0900

    feat: setup nvim & lazyvim

---
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,6 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+vim.keymap.set("i", "jj", "<Esc>")
+vim.keymap.set("i", "<C-a>", "<ESC>I")
+vim.keymap.set("i", "<C-e>", "<End>")
